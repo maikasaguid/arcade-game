5f8978e1b170c8e159561f70d203c4ffa617fca9
maikasaguid <maikasaguid@users.noreply.github.com>
2015-01-15 14:06:39 -1000

    Added character, player subclasses

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ var maxEnemies = 5;
 var lastBlockY = 2;
 var minSpeed = 40;
 var level = 1;
+var paused = 0;
 
 var moves = 0;
 var totalMoves = 0;
@@ -34,15 +35,17 @@ Enemy.prototype.update = function(dt) {
     // all computers.
     var distance = 0;
 
-    if(this.x > 601) {
-        this.reset();
-    }
-    else {
-        distance = this.speed * dt;
-        this.x += distance;
-    }
+    if(!paused) {
+        if(this.x > 601) {
+            this.reset();
+        }
+        else {
+            distance = this.speed * dt;
+            this.x += distance;
+        }
 
-    this.y = this.blockY * imageHeight;
+        this.y = this.blockY * imageHeight;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -69,23 +72,26 @@ Enemy.prototype.reset = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 var Player = function() {
-    this.sprite = 'images/char-boy.png';
+    this.lives = 4;
     this.paddingLeft = 18;
     this.paddingRight = 17;
+    this.sprite = 'images/char-boy.png';
 };
 
 Player.prototype.update = function() {
-    this.x = this.blockX * imageWidth;
-    this.y = this.blockY * imageHeight;
+    if(!paused) {
+        this.x = this.blockX * imageWidth;
+        this.y = this.blockY * imageHeight;
 
-    this.collisionDetection();
+        this.collisionDetection();
 
-    if(this.blockY === 0 && moves >= 5) this.win();
+        if(this.blockY === 0 && moves >= 5) this.winner();
+    }
 };
 
-Player.prototype.win = function() {
-    setTimeout(function() {alert("You've won in " + moves + " moves!");}, 500);
-    setTimeout(this.reset, 3000);
+Player.prototype.winner = function() {
+    ctx.fillText("You've won in " + moves + " moves!", 10, 50);
+    this.reset();
 };
 
 Player.prototype.render = function() {
@@ -127,42 +133,122 @@ Player.prototype.collisionDetection = function() {
 };
 
 Player.prototype.handleInput = function(key) {
-    switch (key) {
-        case 'left':
-            if (this.blockX === 0) this.blockX = 0;
-            else this.blockX -= 1;
-            break;
-        case 'up':
-            if(this.blockY === 0) this.blockY = 0;
-            else this.blockY -= 1;
-            break;
-        case 'right':
-            if(this.blockX === 4) this.blockX = 4;
-            else this.blockX += 1;
-            break;
-        case 'down':
-            if(this.blockY === 5) this.blockY = 5;
-            else this.blockY += 1;
-            break;
+    if(!paused) {
+        switch (key) {
+            case 'left':
+                if (this.blockX === 0) this.blockX = 0;
+                else this.blockX -= 1;
+                break;
+            case 'up':
+                if(this.blockY === 0) this.blockY = 0;
+                else this.blockY -= 1;
+                break;
+            case 'right':
+                if(this.blockX === 4) this.blockX = 4;
+                else this.blockX += 1;
+                break;
+            case 'down':
+                if(this.blockY === 5) this.blockY = 5;
+                else this.blockY += 1;
+                break;
+            case 'restart':
+                gameReset();
+                break;
+            case 'pause':
+                paused = (paused + 1) % 2;
+                break;
+        }
+    }
+    else {
+        switch (key) {
+            case 'restart':
+                gameReset();
+                break;
+            case 'pause':
+                paused = (paused + 1) % 2;
+                break;
+        }
     }
 };
+
+var Boy = function() {
+    Player.call(this);
+
+    this.sprite = 'images/char-boy.png';
+};
+Boy.prototype = Object.create(Player.prototype);
+
+var BoyBig = function() {
+    Player.call(this);
+
+    this.lives = 6;
+    this.paddingLeft = 2;
+    this.paddingRight = 1;
+    this.sprite = 'images/char-big-boy.png';
+};
+BoyBig.prototype = Object.create(Player.prototype);
+
+var GirlCat = function() {
+    Player.call(this);
+
+    this.sprite = 'images/char-cat-girl.png';
+};
+GirlCat.prototype = Object.create(Player.prototype);
+
+var GirlHorn = function() {
+    Player.call(this);
+
+    this.lives = 5;
+    this.paddingLeft = 7;
+    this.sprite = 'images/char-horn-girl.png';
+};
+GirlHorn.prototype = Object.create(Player.prototype);
+
+var GirlPrincess = function() {
+    Player.call(this);
+
+    this.paddingLeft = 14;
+    this.paddingRight = 14;
+    this.sprite = 'images/char-princess-girl.png';
+};
+GirlPrincess.prototype = Object.create(Player.prototype);
+
+var GirlPink = function() {
+    Player.call(this);
+    
+    this.paddingLeft = 14;
+    this.paddingRight = 13;
+    this.sprite = 'images/char-pink-girl.png';
+};
+GirlPink.prototype = Object.create(Player.prototype);
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var player = new Player();
-player.reset();
+var player = new BoyBig();
 
 var allEnemies = [];
 
-for(var i = 0; i < maxEnemies; i++) {
-    allEnemies[i] = new Enemy();
-    allEnemies[i].reset();
+gameReset();
+
+function gameReset() {
+    paused = 0;
+    player.reset();
+
+    for(var i = 0; i < maxEnemies; i++) {
+        allEnemies[i] = new Enemy();
+        allEnemies[i].reset();
+    }
+}
+
+function gameStats() {
+
 }
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
+// Added left handed navigation and R for restart, P for pause
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -172,7 +258,9 @@ document.addEventListener('keyup', function(e) {
         65: 'left',
         87: 'up',
         68: 'right',
-        83: 'down'
+        83: 'down',
+        82: 'restart',
+        80: 'pause'
     };
 
     moves++;
--- js/engine.js
+++ js/engine.js
@@ -172,7 +172,12 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/char-big-boy.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-princess-girl.png',
+        'images/char-pink-girl.png'
     ]);
     Resources.onReady(init);
 
