3a6f13e488eae9c2eb651c7093e1177f58cece57
maikasaguid <maikasaguid@users.noreply.github.com>
2015-02-06 15:21:02 -1000

    Added character select

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ Udacity Front-End Web Developer Nanodegree Project 3
 1. First
 2. Second
 3. Third
+
+##Resources
+Keyboard Keys and Key Code Values
+http://help.adobe.com/en_US/AS2LCR/Flash_10.0/help.html?content=00000520.html
+
+JQuery: Keyup, how do I prevent the default behavior of the arrow (up & down) and enter key?
+http://stackoverflow.com/questions/6220300/jquery-keyup-how-do-i-prevent-the-default-behavior-of-the-arrow-up-down-an
--- js/app.js
+++ js/app.js
@@ -7,7 +7,8 @@ var maxEnemies = 5,
     lastBlockY = 2,
     minSpeed = 40,
     level = 1,
-    paused = 0;
+    paused = 1;
+    mode = 1;
 
 var moves = 0;
 
@@ -60,7 +61,7 @@ Enemy.prototype.update = function(dt) {
 Enemy.prototype.render = function() {
     var offset = 25;
 
-    ctx.drawImage(Resources.get(this.sprite), this.x, (this.y - offset));
+    if(!paused) ctx.drawImage(Resources.get(this.sprite), this.x, (this.y - offset));
 };
 
 //Reset Enemy position
@@ -110,27 +111,27 @@ Player.prototype.winner = function() {
 
 Player.prototype.render = function() {
     var offsetY = 10;
-    var heartImg = 'images/Heart.png';
-    var width = 50;
 
-    for(var i=0; i<this.lives; i++) {
-        ctx.drawImage(Resources.get(heartImg), i * width, 540, width, width);
-    }
+    var heartImg = 'images/Heart.png',
+        heartWidth = 50;
 
     var message = "LEVEL " + this.level;
 
-    ctx2.clearRect(5, (imageHeight * 7) - 60, 100, 25);
-    ctx2.font = "20px Arial";
-    ctx2.lineWidth = 2;
-    ctx2.strokeStyle = "green";
-    ctx2.strokeText(message, 5, (imageHeight * 7) - 40);
-    ctx2.fillStyle = "white";
-    ctx2.fillText("LEVEL " + this.level, 5, (imageHeight * 7) - 40);
+    if(mode == 2) {
+        for(var i=0; i<this.lives; i++) {
+            ctx.drawImage(Resources.get(heartImg), i * heartWidth, 540, heartWidth, heartWidth);
+        }
 
-    ctx.drawImage(Resources.get(this.sprite), this.x, (this.y - offsetY));
+        ctx2.clearRect(5, (imageHeight * 7) - 60, 100, 25);
+        ctx2.font = "20px Arial";
+        ctx2.textAlign = "start";
+        ctx2.lineWidth = 2;
+        ctx2.strokeStyle = "green";
+        ctx2.fillStyle = "white";
+        ctx2.strokeText(message, 5, (imageHeight * 7) - 40);
+        ctx2.fillText(message, 5, (imageHeight * 7) - 40);
 
-    if(this.blockY === 0) {
-        this.reset();
+        ctx.drawImage(Resources.get(this.sprite), this.x, (this.y - offsetY));
     }
 };
 
@@ -160,41 +161,80 @@ Player.prototype.collisionDetection = function() {
 };
 
 Player.prototype.handleInput = function(key) {
-    if(!paused) {
-        switch (key) {
-            case 'left':
-                if (this.blockX === 0) this.blockX = 0;
-                else this.blockX -= 1;
-                break;
-            case 'up':
-                if(this.blockY === 0) this.blockY = 0;
-                else this.blockY -= 1;
-                break;
-            case 'right':
-                if(this.blockX === 4) this.blockX = 4;
-                else this.blockX += 1;
-                break;
-            case 'down':
-                if(this.blockY === 5) this.blockY = 5;
-                else this.blockY += 1;
-                break;
-            case 'restart':
-                gameReset();
-                break;
-            case 'pause':
-                paused = (paused + 1) % 2;
-                break;
-        }
-    }
-    else {
-        switch (key) {
-            case 'restart':
-                gameReset();
-                break;
-            case 'pause':
-                paused = (paused + 1) % 2;
-                break;
-        }
+    switch(mode) {
+        case 1:
+            switch(key) {
+                case '1':
+                    player = new Boy();
+                    break;
+                case '2':
+                    player = new BoyBig();
+                    break;
+                case '3':
+                    player = new GirlCat();
+                    break;
+                case '4':
+                    player = new GirlHorn();
+                    break;
+                case '5':
+                    player = new GirlPrincess();
+                    break;
+                case '6':
+                    player = new GirlPink();
+                    break;
+            }
+
+            if(key >= 1 && key <= 6) {
+                mode = 2;
+                paused = 0;
+                ctx2.clearRect (0, 0, canvas2.width, canvas2.height);
+            }
+
+            break;
+        case 2:
+            if(!paused) {
+                switch(key) {
+                    case 'left':
+                        if (this.blockX === 0) this.blockX = 0;
+                        else this.blockX -= 1;
+                        moves++;
+                        break;
+                    case 'up':
+                        if(this.blockY === 0) this.blockY = 0;
+                        else this.blockY -= 1;
+                        moves++;
+                        break;
+                    case 'right':
+                        if(this.blockX === 4) this.blockX = 4;
+                        else this.blockX += 1;
+                        moves++;
+                        break;
+                    case 'down':
+                        if(this.blockY === 5) this.blockY = 5;
+                        else this.blockY += 1;
+                        moves++;
+                        break;
+                    case 'restart':
+                        gameReset();
+                        break;
+                    case 'pause':
+                        paused = (paused + 1) % 2;
+                        break;
+                }
+            }
+            else {
+                switch(key) {
+                    case 'restart':
+                        gameReset();
+                        break;
+                    case 'pause':
+                        paused = (paused + 1) % 2;
+                        break;
+                }
+            }
+            break;
+        case 3:
+            break;
     }
 };
 
@@ -246,14 +286,13 @@ GirlPink.prototype = Object.create(Player.prototype);
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-var player = new BoyBig();
-
+var player = new Player();
 var allEnemies = [];
 
 gameReset();
 
+
 function gameReset() {
-    paused = 0;
     player.reset();
 
     for(var i = 0; i < maxEnemies; i++) {
@@ -262,15 +301,64 @@ function gameReset() {
     }
 }
 
-function gameStats() {
+function gameChoose() {
+    var allCharacters = [
+        [
+            new Boy(),
+            new GirlHorn()
+        ],
+        [
+            new BoyBig(),
+            new GirlPrincess()
+        ],
+        [
+            
+            new GirlCat(),
+            new GirlPink()
+        ]
+    ];
+    var xOffset = imageWidth,
+        yOffset = imageHeight - 20;
+    var charNum = 1,
+        charX = 0,
+        charY = 0;
+    var message = "Select your character (1-6)";
+
+    
+    ctx2.font = "50px Arial";
+    ctx2.lineWidth = 1;
+    ctx2.strokeStyle = "gray";
+    ctx2.fillStyle = "white";
+
+    for(var y=0; y<2; y++) {
+        for(var x=0; x<3; x++) {
+            charX = xOffset + (x * imageWidth);
+            charY = yOffset + (y * rawImageHeight);
+
+            ctx2.drawImage(Resources.get(allCharacters[x][y].sprite), charX, charY);
+
+            ctx2.fillText(charNum, charX, charY + 150);
+            ctx2.strokeText(charNum, charX, charY + 150);
+
+            charNum++;
+        }
+    }
+
+    ctx2.font = "bold 36px Arial";
+    ctx2.textAlign = "center";
+    ctx2.fillStyle = "black";
+    ctx2.fillText(message, canvas2.width / 2, charY + 35);
+}
+
+function gameEnd() {
 
 }
 
-
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-// Added left handed navigation and R for restart, P for pause
-document.addEventListener('keyup', function(e) {
+// Also includes left handed navigation, numbers 1-6, R for restart, and P for pause
+// Prevents window scroll for smaller screens
+document.addEventListener('keydown', function(e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
@@ -281,9 +369,19 @@ document.addEventListener('keyup', function(e) {
         68: 'right',
         83: 'down',
         82: 'restart',
-        80: 'pause'
+        80: 'pause',
+        13: 'enter',
+        49: '1',
+        50: '2',
+        51: '3',
+        52: '4',
+        53: '5',
+        54: '6'
     };
 
-    moves++;
+    if([37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+
     player.handleInput(allowedKeys[e.keyCode]);
 });
